e6472f992aee1991eb25a7996bf552d348627df6
Quesdtion <quesstionant@gmail.com>
2025-11-20 17:42:46 +0200

    Update status_mark.py

--- bot/handlers/status_mark.py
+++ bot/handlers/status_mark.py
@@ -1,5 +1,10 @@
 from aiogram import Router, F
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import (
+    Message,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardRemove,
+)
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
